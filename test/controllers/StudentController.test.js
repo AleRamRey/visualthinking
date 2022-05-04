1824117ca892ce3a99599d247a70ea9cc5f4727d
AleRamRey <launchx09432@innovaccion.mx>
2022-05-04 14:48:49 -0500

    Se agrega set de pruebas de StudentController.test

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -2,11 +2,19 @@ const StudentController = require("./../../lib/controllers/StudentController");
 
 describe("Test for StudentController", () => {
 
-    const students = Reader.readJasonFile("students.json");
-
     test("1. Obtiene todos los estudiantes", () => {
-        const studentsAll = StudentController.getAllStudents(students);
-        expect(studentsAll.length).toBe(50);
+        const students = StudentController.getAllStudents();
+        expect(students.length).toBe(51);
+    }),
+
+    test("2. Obtiene los emails de los estudiantes con certificacion", () => {
+        const students = StudentController.getStudentsWithCertificacion();
+        expect(students.length).toBe(29);
+    }),
+
+    test("3. Obtiene los estudiantes con creditos mayores de 500",()=>{
+        const students = StudentController.getStudentsWithMoreThan500Credits();
+        expect(students.length).toBe(27);        
     })
 
 });
